9a0070a07edac314db660a514f1cabab2dab16c5
Ainsley Nguyen <nhnanh2508@gmail.com>
2025-12-01 11:23:40 +0700

    OneR and Random Forest algorithm

---
+++ src/basemodel/RandomForestTuning.java
@@ -0,0 +1,51 @@
+package basemodel;
+
+import weka.classifiers.evaluation.Evaluation;
+import weka.classifiers.trees.RandomForest;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils;
+import java.util.Random;
+import weka.core.SerializationHelper;
+
+public class RandomForestTuning {
+    public static void main(String[] args) throws Exception {
+        long startTime = System.nanoTime();
+
+        ConverterUtils.DataSource source = new ConverterUtils.DataSource("data\\heart_disease_cleaned.arff");
+        Instances dataset = source.getDataSet();
+
+        dataset.setClassIndex(dataset.numAttributes() - 1);
+
+        String[] options = new String[4];
+        options[0] = "-P"; options[1] = "50";
+        options[2] = "-I"; options[3] = "50";
+
+        // Create and train the RandomForest classifier
+        RandomForest randomForest = new RandomForest();
+        randomForest.setOptions(options);
+        randomForest.buildClassifier(dataset);
+        System.out.println("RandomForest Selected Parameters: " + String.join(" ", randomForest.getOptions()));
+
+        Evaluation eval = new Evaluation(dataset);
+        eval.crossValidateModel(randomForest, dataset, 10, new Random(42));
+
+        // Print the confusion matrix
+        System.out.println("Confusion Matrix:\n" + eval.toMatrixString());
+
+        // Print additional evaluation metrics
+        System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+        System.out.println("Precision = " + eval.precision(1));
+        System.out.println("Recall = " + eval.recall(1));
+        System.out.println("F-Measure = " + eval.fMeasure(1));
+        System.out.println("Error Rate = " + eval.errorRate());
+        System.out.println(eval.toClassDetailsString());
+
+        SerializationHelper.write("models\\RandomForest.model", randomForest);
+
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
+        System.out.println("Runtime: " + duration + " nanoseconds");
+    }
+}
